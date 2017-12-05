e1a6e1be64ccce0fc7f8f7b8ea49eec9a0c33302
Surajit Sarkar <surajit@bitcanny.com>
2017-12-05 19:25:07 +0530

    minor changes

--- main.py
+++ main.py
@@ -6,19 +6,20 @@ p = person.Person("srs")
 p.say_hi()
 
 
-droid1 = Robot("R2-D2")
-droid1.say_hi()
-Robot.how_many()
 
-droid2 = Robot("C-3PO")
-droid2.say_hi()
-Robot.how_many()
-
-print("\nRobots can do some work here.\n")
-
-print("Robots have finished their work. So let's destroy them.")
-droid1.die()
-droid2.die()
-
-Robot.how_many()
+# droid1 = Robot("R2-D2")
+# droid1.say_hi()
+# Robot.how_many()
+#
+# droid2 = Robot("C-3PO")
+# droid2.say_hi()
+# Robot.how_many()
+#
+# print("\nRobots can do some work here.\n")
+#
+# print("Robots have finished their work. So let's destroy them.")
+# droid1.die()
+# droid2.die()
+#
+# Robot.how_many()
 
--- person.py
+++ person.py
@@ -1,6 +1,8 @@
 class Person:
-    def __init__(self, name):
+
+    def __init__(self, name, age = -1):
         self.name = name
+        self.age = age
 
     def say_hi(self):
         print('Hello, my name is', self.name)
